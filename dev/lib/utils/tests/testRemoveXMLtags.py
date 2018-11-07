9ae9f28c932120986b6fce3fc8b3d9235e771c5a
JesenTanadi <jesentanadi@JesenTanadi.local>
2018-11-07 10:09:11 -0500

    rename

--- dev/lib/utils/tests/testRemoveXMLtags.py
+++ dev/lib/utils/tests/testRemoveXMLtags.py
@@ -1,10 +1,18 @@
 from lib.utils.convertProofToPreset import removeXMLtags
 import unittest
 
-class TestRemoveXMLTags(unittest.TestCase):
+class TestRemoveXMLtags(unittest.TestCase):
     def test_base(self):
+        """Base case"""
         taggedString = "<group>UC, lc, etc.</group>"
         cleanedString = removeXMLtags(taggedString, "group")
         expected = "UC, lc, etc."
 
         self.assertEqual(cleanedString, expected)
+
+    def test_noClosingTag(self):
+        """In case someone forgets to use a closing tag"""
+        taggedString = "<group>Upper case<group>"
+
+if __name__ == "__main__":
+    unittest.main(exit=False, verbosity=1)
