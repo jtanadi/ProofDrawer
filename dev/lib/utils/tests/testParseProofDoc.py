c405493a502e4173519e20fb469eeccd86d51c87
JesenTanadi <jesentanadi@JesenTanadi.local>
2018-11-07 12:32:09 -0500

    initial commit base test

---
+++ dev/lib/utils/tests/testParseProofDoc.py
@@ -0,0 +1,72 @@
+from lib.utils.convertProofToPreset import parseProofDoc
+import os.path
+import unittest
+
+class TestParseProofDoc(unittest.TestCase):
+    def test_baseAsFile(self):
+        """
+        Base case. The file content looks like this:
+        <group>UC, lc, numerals</group>
+        ABCDEFGHIJKLMNOPQRSTUVWXYZ
+        abcdefghijklmnopqrstuvwxyz
+        0123456789
+
+        <group>UC control</group>
+        |H| |O| HOHOHOHO
+        |A| HAHAHAOAOAOA
+        """
+        currentFile = os.path.dirname(__file__)
+        proofDoc = os.path.join(currentFile, "resources", "proofDocTest.txt")
+        expected = [
+            {
+                "group": "UC, lc, numerals",
+                "contents": [
+                    "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+                    "abcdefghijklmnopqrstuvwxyz",
+                    "0123456789"
+                ]
+            },
+            {
+                "group": "UC control",
+                "contents": [
+                    "|H| |O| HOHOHOHO",
+                    "|A| HAHAHAOAOAOA"
+                ]
+            }
+        ]
+        self.assertEqual(parseProofDoc(proofDoc, "group"), expected)
+    
+    def test_baseAsList(self):
+        """
+        Base case, using list instead of file
+        """
+        proofList = [
+            "<group>UC, lc, numerals</group>",
+            "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+            "abcdefghijklmnopqrstuvwxyz",
+            "0123456789",
+            "<group>UC control</group>",
+            "|H| |O| HOHOHOHO",
+            "|A| HAHAHAOAOAOA"
+            ]
+        expected = [
+            {
+                "group": "UC, lc, numerals",
+                "contents": [
+                    "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+                    "abcdefghijklmnopqrstuvwxyz",
+                    "0123456789"
+                ]
+            },
+            {
+                "group": "UC control",
+                "contents": [
+                    "|H| |O| HOHOHOHO",
+                    "|A| HAHAHAOAOAOA"
+                ]
+            }
+        ]
+        self.assertEqual(parseProofDoc(proofList, "group"), expected)
+
+if __name__ == "__main__":
+    unittest.main(exit=False, verbosity=1)
