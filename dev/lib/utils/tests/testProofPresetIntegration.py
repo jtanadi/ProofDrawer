bc048a7dd8d84f36e30c0bcfe625a4b3f843de50
Jesen Tanadi <mail@jesentanadi.com>
2018-11-08 07:27:41 -0500

    test proof2preset flow

---
+++ dev/lib/utils/tests/testProofPresetIntegration.py
@@ -0,0 +1,16 @@
+from lib.utils.proofPreset import ProofPreset
+from lib.utils.readWritePreset import readJSONpreset, writeJSONpreset
+import os.path
+
+currentDir = os.path.dirname(__file__)
+proofFileDir = os.path.join(currentDir, "resources", "proofDocTest.txt")
+
+proofFile = open(proofFileDir, "r")
+proofList = proofFile.readlines()
+proofFile.close()
+
+proofPreset = ProofPreset(proofList, "group")
+presetList = proofPreset.getPreset()
+
+presetFileDir = os.path.join(currentDir, "resources", "proofPresetTest.json")
+writeJSONpreset(presetFileDir, presetList)
