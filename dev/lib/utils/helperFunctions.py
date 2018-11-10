738c6e6e6f026877109343d4a142b98510fb4b23
Jesen Tanadi <mail@jesentanadi.com>
2018-11-10 16:26:21 -0500

    test HF funcs, refactor to comprehensions

--- dev/lib/utils/helperFunctions.py
+++ dev/lib/utils/helperFunctions.py
@@ -11,12 +11,11 @@ def getValuesFromListOfDicts(listOfDicts, keyToSearch):
 
 def convertToListOfPyDicts(listOfNSDicts):
     """
+    Return a list of Py dicts
+
     Since dicts in vanilla.List() are converted to __NSDictionaryM,
     we have to transfer them back to Py dicts for some internal funcs,
-    like writing to JSON, internal dict manipulations, etc..
+    like writing to JSON, internal dict manipulations, etc.
     """
-    returnList = []
-    for dictItem in listOfNSDicts:
-        returnList.append({key:value for (key, value) in dictItem.items()})
-
-    return returnList
+    return [{key:value for (key, value) in dictItem.items()}\
+            for dictItem in listOfNSDicts]
--- dev/lib/utils/tests/testHF.py
+++ dev/lib/utils/tests/testHF.py
@@ -2,5 +2,68 @@
 Test helper functions
 """
 
-from utils.helperFunctions import *
+import utils.helperFunctions as hf
 import unittest
+
+class TestHelperFunctions(unittest.TestCase):
+    def setUp(self):
+        self.inputList = [
+            {
+                "key1": "key1Val1",
+                "key2": "key2Val1",
+                "key3": "key3Val1",
+                "key4": "key4Val1"
+            },
+            {
+                "key1": "key1Val2",
+                "key2": "key2Val2",
+                "key3": "key3Val2",
+                "key4": "key4Val2"
+            },
+            {
+                "key1": "key1Val3",
+                "key2": "key2Val3",
+                "key3": "key3Val3",
+                "key4": "key4Val3"
+            }
+        ]
+
+    def test_baseGetValuesFromListOfDicts(self):
+        """
+        Base case for hf.getValuesFromListOfDicts()
+        """
+        actual = hf.getValuesFromListOfDicts(self.inputList, "key1")
+        expected = ["key1Val1", "key1Val2", "key1Val3"]
+
+        self.assertEqual(actual, expected)
+
+    def test_baseConvertToListOfPyDicts(self):
+        """
+        Base case for hf.convertToListOfPyDicts()
+        """
+        actual = hf.convertToListOfPyDicts(self.inputList)
+        expected = [
+            {
+                "key1": "key1Val1",
+                "key2": "key2Val1",
+                "key3": "key3Val1",
+                "key4": "key4Val1"
+            },
+            {
+                "key1": "key1Val2",
+                "key2": "key2Val2",
+                "key3": "key3Val2",
+                "key4": "key4Val2"
+            },
+            {
+                "key1": "key1Val3",
+                "key2": "key2Val3",
+                "key3": "key3Val3",
+                "key4": "key4Val3"
+            }
+        ]
+        self.assertEqual(actual, expected)
+
+
+if __name__ == "__main__":
+    unittest.main(exit=False, verbosity=1)
