332835a5cf854ed30684ec7a75aa3f17c4d0a538
Jesen Tanadi <mail@jesentanadi.com>
2018-11-11 21:10:52 -0500

    passing flow test

--- dev/lib/utils/tests/testProofPresetFlow.py
+++ dev/lib/utils/tests/testProofPresetFlow.py
@@ -2,18 +2,18 @@
 Test work flow...
 """
 
-from lib.utils.proofPreset import ProofPreset
-from lib.utils.readWritePreset import readJSONpreset, writeJSONpreset
+from utils.proofPreset import ProofPreset
+from utils.readWritePreset import readJSONpreset, writeJSONpreset
 import os.path
 
 currentDir = os.path.dirname(__file__)
 proofFileDir = os.path.join(currentDir, "resources", "proofDocTest.txt")
 
-proofFile = open(proofFileDir, "r")
-proofList = proofFile.readlines()
-proofFile.close()
+with open(proofFileDir, "r") as proofFile:
+    proofList = proofFile.readlines()
 
-proofPreset = ProofPreset(proofList, "group")
+proofPreset = ProofPreset("proofPreset1")
+proofPreset.importProof(proofList, "group")
 presetList = proofPreset.getPreset()
 
 presetFileDir = os.path.join(currentDir, "resources", "proofPresetTest.json")
