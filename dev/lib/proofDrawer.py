8b68eaa503f3359d62a32aa9761f456ad8e2d480
Jesen Tanadi <mail@jesentanadi.com>
2018-11-21 07:14:27 -0500

    use and modify ProofPreset() objects

--- dev/lib/proofDrawer.py
+++ dev/lib/proofDrawer.py
@@ -18,17 +18,15 @@ class ProofDrawer:
         self.presetsList = presetsList
 
         self.currentPreset = self.presetsList[0]
-        self.presetNames = []
-        self._getPresetNames()
+        self.presetNamesList = self._getPresetNames()
 
+        # These 3 might be up for deletion
         self.proofGroupInspector = None
         self.editedGroupIndex = None
-
         self.listHasBeenEdited = False # A flag for later... see closeWindowCB()
-        
+
         self._buildUI()
         self._refreshProofGroups()
-        
 
         addObserver(self, "_inspectorClosed", "comInspectorClosed")
         addObserver(self, "_updateProofGroup", "comProofGroupEdited")
@@ -71,10 +69,6 @@ class ProofDrawer:
             }
         ]
 
-        # Add order number for our list
-        # for index, group in enumerate(self.currentPreset.getGroups()):
-        #     group["order"] = index + 1
-
         width = 425
         left = 10
         row = 10
@@ -99,7 +93,7 @@ class ProofDrawer:
                                     alignment="right")
 
         self.w.presetsList = PopUpButton((popUpLeft, row, presetsPopUpWidth, textHeight),
-                                         items=self.presetNames,
+                                         items=self.presetNamesList,
                                          callback=self.setCurrentPresetCB)
 
         self.w.editPresets = ImageButton((width - 38, row, 22, 22),
@@ -113,11 +107,11 @@ class ProofDrawer:
         row += 15
         self.w.proofGroups = List((left + 3, row, listWidth, 255),
                                   rowHeight=18,
-                                  items=self.currentPreset.getGroups(),
+                                  items=[],
                                   columnDescriptions=listForList,
                                   allowsSorting=False,
                                   allowsMultipleSelection=False,
-                                  editCallback=self.editProofGroupsCB)
+                                  editCallback=self.editProofGroupCB)
 
         buttonGroup1Left = popUpLeft + presetsPopUpWidth + 3
         buttonGroup1Top = row + 58
@@ -143,19 +137,19 @@ class ProofDrawer:
         self.w.line2 = HorizontalLine((left, row, -10, 1))
 
         row += 10
-        self.w.additionalGroupsText = TextBox((left, row, -10, 20),
+        self.w.additionalGroupNamesText = TextBox((left, row, -10, 20),
                                               "Add more proof groups:")
 
         row += 25
-        self.w.additionalGroups = List((left + 3, row, listWidth, 150),
-                                       rowHeight=17,
-                                       items=self.currentPreset.getGroupNames(returnCopies=False),
-                                       allowsSorting=False,
-                                       allowsMultipleSelection=False)
+        self.w.additionalGroupNames = List((left + 3, row, listWidth, 150),
+                                           rowHeight=17,
+                                           items=self.currentPreset.getGroupNames(returnCopies=False),
+                                           allowsSorting=False,
+                                           allowsMultipleSelection=True)
 
         self.w.addGroup = Button((buttonGroup1Left, row + 60, 30, 20),
                                  "+",
-                                 callback=self.addProofGroupCB)
+                                 callback=self.addProofGroupsCB)
 
         # row += 25
         # self.w.previewButton = Button((additionalWidth + 20, row, -10, 20),
@@ -168,8 +162,10 @@ class ProofDrawer:
         #                              callback=self.testerCB)
 
     def _getPresetNames(self):
-        for preset in self.presetsList:
-            self.presetNames.append(preset.getPresetName())
+        """
+        Get preset names for display
+        """
+        return [preset.getPresetName() for preset in self.presetsList]
 
     def _uiEnabled(self, onOff=True):
         """
@@ -184,7 +180,7 @@ class ProofDrawer:
         self.w.moveGroupUP.enable(onOff)
         self.w.moveGroupDN.enable(onOff)
         self.w.removeGroup.enable(onOff)
-        self.w.additionalGroups.enable(onOff)
+        self.w.additionalGroupNames.enable(onOff)
         self.w.addGroup.enable(onOff)
 
     def _inspectorClosed(self, info):
@@ -204,16 +200,18 @@ class ProofDrawer:
         """
         self.w.proofGroups[self.editedGroupIndex] = info["newProofGroup"]
 
-    def _refreshProofGroups(self):
+    def _refreshProofGroups(self, newSelection=0):
         """
-        Refresh self.w.proofGroups and
-        set order numbers
+        Refresh the proof groups list, set order numbers,
+        and set selection.
+
+        newSelection defaults to first index in list.
         """
-        # Set flag so editProofGroupsCB() isn't
+        # Set flag so editProofGroupCB() isn't
         # called when set proofGroups contents &
         # set order numbers
         self._proofReadyToEdit = False
-        
+
         self.w.proofGroups.set(self.currentPreset.getGroups())
 
         newOrder = 1
@@ -222,18 +220,17 @@ class ProofDrawer:
             newOrder += 1
 
         self._proofReadyToEdit = True
+        self.w.proofGroups.setSelection([newSelection])
 
     def fontButtonCB(self, sender):
-        # pass
         selectedFont = self.fonts[sender.get()]
         self.w.setTitle("Proof Drawer: %s" % selectedFont)
 
     def setCurrentPresetCB(self, sender):
-        selectedIndex = sender.get()
-        self.currentPreset = self.presetsList[selectedIndex]
-        self.w.proofGroups.set(self.currentPreset.getGroups())
-        self.w.additionalGroups.set(self.currentPreset.getGroupNames(returnCopies=False))
+        selectedPresetIndex = sender.get()
+        self.currentPreset = self.presetsList[selectedPresetIndex]
         self._refreshProofGroups()
+        self.w.additionalGroupNames.set(self.currentPreset.getGroupNames(returnCopies=False))
 
     def inspectGroupCB(self, sender):
         """
@@ -252,7 +249,13 @@ class ProofDrawer:
         self.proofGroupInspector.w.makeKey()
         self._uiEnabled(False)
 
-    def editProofGroupsCB(self, sender):
+    def editProofGroupCB(self, sender):
+        """
+        Edit selected proof group and refresh proof groups list.
+
+        ProofPreset.editGroup() is called on a field-by-field basis
+        so we're not trying to edit "name" when only editing "leading"
+        """
         # Don't do anything if proof groups aren't
         # ready to be edited or if nothing is selected
         if not self._proofReadyToEdit or\
@@ -261,6 +264,7 @@ class ProofDrawer:
 
         selectedIndex = self.w.proofGroups.getSelection()[0]
 
+        # Capture current values for checking later
         currentGroup = self.currentPreset.getGroups()[selectedIndex]
         currentName = currentGroup["name"]
         currentPrint = currentGroup["print"]
@@ -270,36 +274,39 @@ class ProofDrawer:
         newName = self.w.proofGroups[selectedIndex]["name"]
         newPrint = self.w.proofGroups[selectedIndex]["print"]
 
+        # ProofPreset.editGroup() will only accept floats for
+        # typeSize and leading
+        try:
+            newSize = float(self.w.proofGroups[selectedIndex]["typeSize"])
+        except ValueError:
+            newSize = currentSize
+        try:
+            newLeading = float(self.w.proofGroups[selectedIndex]["leading"])
+        except ValueError:
+            newLeading = currentLeading
+
+        # "Target" editGroup() more precisely, so we're not constantly
+        # calling editGroup() on all the other values if we've only edited one
+        # For ex. if calling editGroup() on everything, when only editing leading,
+        # we'll get a ValueError("Name already exists") because it'll try to
+        # update current group name with the same "new" name
         if newName != currentName:
             self.currentPreset.editGroup(selectedIndex, name=newName)
         if newPrint != currentPrint:
             self.currentPreset.editGroup(selectedIndex, print=newPrint)
+        if newSize != currentSize:
+            self.currentPreset.editGroup(selectedIndex, typeSize=newSize)
+        if newLeading != currentLeading:
+            self.currentPreset.editGroup(selectedIndex, leading=newLeading)
 
-        # float conversion should prob happen in editGroup?
-        try:
-            newSize = float(self.w.proofGroups[selectedIndex]["typeSize"])
-            if newSize != currentSize:
-                self.currentPreset.editGroup(selectedIndex, typeSize=newSize)
-        except ValueError:
-            self.w.proofGroups[selectedIndex]["typeSize"] = currentSize
-
-        try:
-            newLeading = float(self.w.proofGroups[selectedIndex]["leading"])
-            if newLeading != currentLeading:
-                self.currentPreset.editGroup(selectedIndex, leading=newLeading)
-        except ValueError:
-            self.w.proofGroups[selectedIndex]["leading"] = currentLeading
-
-        self._refreshProofGroups()
+        self._refreshProofGroups(selectedIndex)
 
     def moveGroupCB(self, sender):
         """
-        Both up and down buttons call this method because
-        they share the same sorting logic.
+        Move selected group and refresh proof groups list.
 
-        The sorting works by holding the selected object
-        in a temp variable, deleting from the groups list,
-        and then re-inserting in the index before or after.
+        Both up and down buttons call this method because they
+        both call ProofPreset.moveGroup(currentIndex, new Index)
         """
         if not self.w.proofGroups or not self.w.proofGroups.getSelection():
             return
@@ -319,32 +326,29 @@ class ProofDrawer:
             newIndex = currentIndex + 1
 
         self.currentPreset.moveGroup(currentIndex, newIndex)
-        self._refreshProofGroups()
-        self.w.proofGroups.setSelection([newIndex])
+        self._refreshProofGroups(newSelection=newIndex)
 
     def removeGroupCB(self, sender):
         """
-        Delete selected and refresh order number.
+        Delete selected group and refresh proof groups list
         """
         if not self.w.proofGroups or not self.w.proofGroups.getSelection():
             return
-        
-        selectionIndex = self.w.proofGroups.getSelection()[0]
-        self.currentPreset.removeGroup(selectionIndex)
-        self._refreshProofGroups()
-        self.w.proofGroups.setSelection([selectionIndex])
+        groupToDeleteIndex = self.w.proofGroups.getSelection()[0]
 
-    def addProofGroupCB(self, sender):
+        self.currentPreset.removeGroup(groupToDeleteIndex)
+        self._refreshProofGroups(newSelection=groupToDeleteIndex) # This will select next group
+
+    def addProofGroupsCB(self, sender):
         """
-        Append selected additional group to main list
-        and add some information along the way.
+        Add new groups from additionalGroupNames list
         """
-        selectionIndices = self.w.additionalGroups.getSelection()
-        if not selectionIndices:
+        newGroupNamesIndices = self.w.additionalGroupNames.getSelection()
+        if not newGroupNamesIndices:
             return
 
-        for index in selectionIndices:
-            groupToAdd = {"name": self.w.additionalGroups[index]}
+        for index in newGroupNamesIndices:
+            groupToAdd = {"name": self.w.additionalGroupNames[index]}
             self.currentPreset.addGroup(groupToAdd)
 
         self._refreshProofGroups()
