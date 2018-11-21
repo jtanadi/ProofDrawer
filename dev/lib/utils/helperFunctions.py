ee2189e75e9fff1bf63ff9cbf4fb952c18c3b160
Jesen Tanadi <mail@jesentanadi.com>
2018-11-21 08:36:18 -0500

    add new function

--- dev/lib/utils/helperFunctions.py
+++ dev/lib/utils/helperFunctions.py
@@ -2,6 +2,16 @@
 Helper functions
 """
 
+def makeCleanListFromStr(contentString):
+    """
+    Return a clean list from passed-in contentString
+
+    Clean means no empty items and no leading and
+    trailing whitespaces
+    """
+    tempList = contentString.split("\n")
+    return [item.strip() for item in tempList if item.strip()]
+
 def getValuesFromListOfDicts(listOfDicts, keyToSearch):
     """
     Return a list of values of keyToSearch from a list of dicts
